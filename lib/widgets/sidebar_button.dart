a77cbd992db55a4b4819577cb7bb6b83d0f9c045
aami1421 <aqeelalmosa14@gmail.com>
2023-02-15 18:44:06 +0300

    sidebar

---
+++ lib/widgets/sidebar_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SidebarButton extends StatelessWidget {
+  final String btnName;
+  final String? routeName;
+  bool selected;
+
+  SidebarButton(
+      {super.key,
+      required this.btnName,
+      this.routeName,
+      this.selected = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 40,
+          color: Color.fromRGBO(0, 0, 0, 0),
+          margin: EdgeInsets.symmetric(horizontal: 3, vertical: 2),
+          child: ElevatedButton(
+            onPressed: () => null,
+            child: Text(btnName,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: selected
+                        ? Color.fromRGBO(255, 255, 255, 1)
+                        : Color.fromRGBO(34, 22, 112, 0.6))),
+            style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(selected
+                    ? Color.fromRGBO(34, 22, 112, 0.6)
+                    : Color.fromRGBO(255, 255, 255, 1)),
+                alignment: Alignment.centerLeft),
+          ),
+        ),
+      ],
+    );
+  }
+}
