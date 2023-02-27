2a342cd0454207c5c74322effdcddc2bd7220e82
aami1421 <aqeelalmosa14@gmail.com>
2023-02-27 18:23:21 +0300

    edit profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -15,60 +15,65 @@ class _profileScreenState extends State<profileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const CustomDrawer(),
         body: Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 30),
-                child: CircleAvatar(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 50, bottom: 30, right: 50),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
                   radius: 50,
                   child: ClipOval(
-                    child: Image(image: AssetImage('/avatarPlaceholder.png')),
+                    child: Image(
+                      image: AssetImage('/avatarPlaceholder.png'),
+                      width: 200,
+                      height: 200,
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
                     'Company Name',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Email',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Phone Number',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )
-                ],
-              ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Email',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              )
             ],
           ),
-        ));
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Phone Number',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              )
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
