22a1dbf93397d3c6bc70b531ad7a4ba5b1b95562
aami1421 <aqeelalmosa14@gmail.com>
2023-03-28 11:46:38 +0300

    edit jobs list UI

--- lib/screens/jobs_list.dart
+++ lib/screens/jobs_list.dart
@@ -76,125 +76,131 @@ class _JobsListState extends State<JobsList> {
         alignment: Alignment.centerLeft,
         height: 100,
         width: 200,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 80,
-              child: Text(
-                "${widget.job.dt!.toLocal()}".substring(0, 11) +
-                    "\n${widget.job.dt!.toLocal().hour}:${widget.job.dt!.toLocal().minute}:${widget.job.dt!.toLocal().second}",
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              width: 100,
-              child: Text(
-                widget.job.jobTitle,
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              width: 80,
-              child: Text(
-                widget.job.yearsOfExperience,
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              width: 80,
-              child: Text(
-                widget.job.major,
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              width: 80,
-              child: Text(
-                widget.job.city != '' ? widget.job.city : "not specified",
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              width: 200,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
+        child: TextButton(
+          onPressed: () {
+            print("clicked..");
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 80,
                 child: Text(
-                  widget.job.descreption.length > MAX_CHARS
-                      ? widget.job.descreption.substring(0, 80) + " ..show more"
-                      : widget.job.descreption,
+                  "${widget.job.dt!.toLocal()}".substring(0, 11) +
+                      "\n${widget.job.dt!.toLocal().hour}:${widget.job.dt!.toLocal().minute}:${widget.job.dt!.toLocal().second}",
                   style: const TextStyle(fontSize: 15),
                 ),
               ),
-            ),
-            SizedBox(
-              width: 150,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: widget.job.isActive
-                            ? (isHovering
-                                ? Colors.green.shade400
-                                : Colors.green)
-                            : (isHovering ? Colors.red : Colors.red.shade300),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: TextButton(
-                      child: isLoading
-                          ? const CircularProgressIndicator()
-                          : widget.job.isActive
-                              ? const Text(
-                                  "Deactivate",
-                                  style: TextStyle(color: Colors.white),
-                                )
-                              : const Text(
-                                  "Activate",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                      onPressed: () async {
-                        setState(() {
-                          isLoading = true;
-                        });
-                        await Post().toggleStatus(widget.job.id);
-                        setState(() {
-                          isLoading = false;
-                          widget.job.isActive = !widget.job.isActive;
-                        });
-                      },
-                      onHover: (value) {
-                        setState(() {
-                          isHovering = value;
-                        });
-                      },
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: TextButton(
-                      child: deleteIsLoading
-                          ? const CircularProgressIndicator()
-                          : const Text(
-                              "Delete",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                      onPressed: () async {
-                        await _deleteConfirm(widget.job.id);
-                      },
-                      onHover: (value) {
-                        setState(() {
-                          deleteIsHovering = value;
-                        });
-                      },
-                    ),
-                  ),
-                ],
+              SizedBox(
+                width: 100,
+                child: Text(
+                  widget.job.jobTitle,
+                  style: const TextStyle(fontSize: 15),
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                width: 80,
+                child: Text(
+                  widget.job.yearsOfExperience,
+                  style: const TextStyle(fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                width: 80,
+                child: Text(
+                  widget.job.major,
+                  style: const TextStyle(fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                width: 80,
+                child: Text(
+                  widget.job.city != '' ? widget.job.city : "not specified",
+                  style: const TextStyle(fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                width: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text(
+                    widget.job.descreption.length > MAX_CHARS
+                        ? widget.job.descreption.substring(0, 80) +
+                            " ..show more"
+                        : widget.job.descreption,
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: widget.job.isActive
+                              ? (isHovering
+                                  ? Colors.green.shade400
+                                  : Colors.green)
+                              : (isHovering ? Colors.red : Colors.red.shade300),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: TextButton(
+                        child: isLoading
+                            ? const CircularProgressIndicator()
+                            : widget.job.isActive
+                                ? const Text(
+                                    "Deactivate",
+                                    style: TextStyle(color: Colors.white),
+                                  )
+                                : const Text(
+                                    "Activate",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                        onPressed: () async {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          await Post().toggleStatus(widget.job.id);
+                          setState(() {
+                            isLoading = false;
+                            widget.job.isActive = !widget.job.isActive;
+                          });
+                        },
+                        onHover: (value) {
+                          setState(() {
+                            isHovering = value;
+                          });
+                        },
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: TextButton(
+                        child: deleteIsLoading
+                            ? const CircularProgressIndicator()
+                            : const Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
+                        onPressed: () async {
+                          await _deleteConfirm(widget.job.id);
+                        },
+                        onHover: (value) {
+                          setState(() {
+                            deleteIsHovering = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
